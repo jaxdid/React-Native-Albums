2225b19d374252f54dd56cbfeff034cba876c90b
Alain Lemaire <jaxdid@gmail.com>
2017-04-17 21:21:28 +0100

    Adds album header and image content and makes list scrollable

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,58 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { artist, thumbnail_image, image, title } = album
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={styles.thumbnailStyle}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={styles.imageStyle}
+        />
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    width: null,
+    height: 300,
+    flex: 1
+  }
 }
 
 export default AlbumDetail
